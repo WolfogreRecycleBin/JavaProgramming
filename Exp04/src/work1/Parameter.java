172d16613d2c3190509eaa9fcecb0d852ac5c7d8
Jason Song <wolfogre@outlook.com>
2015-12-20 01:22:52 +0800

    Deleted all, ready for IntelliJ IDEA

--- Exp04/src/work1/Parameter.java
+++
@@ -1,9 +0,0 @@
-package work1;
-
-public class Parameter {
-	public double a,b;
-	public Parameter(double a, double b){
-		this.a = a;
-		this.b = b;
-	}
-}
--- Gobang/src/shu/gobang/server/TestGobangSever_Sever.java
+++
@@ -1,8 +0,0 @@
-package shu.gobang.server;
-
-public class TestGobangSever_Sever {
-	public static void main(String args[]) throws Exception{
-		GobangServer gbs = new GobangServer(6789);
-		gbs.startServer();
-	}
-}

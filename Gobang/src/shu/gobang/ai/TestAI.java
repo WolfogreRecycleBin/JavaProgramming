9a43462b1f09536753ef94eaad96e5195fc16693
Jason Song <wolfogre@outlook.com>
2015-12-25 00:32:59 +0800

    Added Umpire

--- Gobang/src/shu/gobang/ai/TestAI.java
+++ Gobang/src/shu/gobang/ai/TestAI.java
@@ -8,18 +8,26 @@ import java.util.Scanner;
 public class TestAI {
 	public static void main(String args[]){
 		AI ai = new AI();
+		Umpire umpire = new Umpire();
 		Scanner scanner = new Scanner(System.in);
 		int [][] board = new int [15][15];
-		AI.Winner winner;
-		while((winner = ai.move(board,1,2)).equals(AI.Winner.UNKNOWN)){
+		boolean myTurn = true;
+		while(umpire.getWinner(board,1,2).equals(Umpire.Winner.UNKNOWN)){
+			if(myTurn) {
+				System.out.println("Please move:");
+				board[scanner.nextInt()][scanner.nextInt()] = 1;
+				myTurn = false;
+			}else{
+				System.out.println("AI moving...");
+				ai.move(board,1,2);
+				myTurn = true;
+			}
 			for(int[] vs : board) {
 				for (int v : vs)
 					System.out.print(v + " ");
 				System.out.println();
 			}
-			System.out.println("Please move:");
-			board[scanner.nextInt()][scanner.nextInt()] = 1;
 		}
-		System.out.println("Winner:" + winner);
+		System.out.println("Winner:" + umpire.getWinner(board,1,2));
 	}
 }
---
+++ Gobang/src/shu/gobang/ai/Umpire.java
@@ -0,0 +1,55 @@
+package shu.gobang.ai;
+
+/**
+ * Created by Jason Song(wolfogre@outlook.com) on 12/25/2015.
+ */
+public class Umpire {
+	public enum Winner {PLAYER1, PLAYER2, UNKNOWN, NONE;}
+
+	int [][] board;
+
+	public Winner getWinner(int [][] board, int player1Chess, int player2Chess){
+		this.board = board;
+		if(checkWinner(player1Chess))
+			return Winner.PLAYER1;
+		if(checkWinner(player2Chess))
+			return Winner.PLAYER2;
+		if(isFull())
+			return Winner.NONE;
+		return Winner.UNKNOWN;
+	}
+
+	private boolean checkWinner(int who){
+		for(int row = 0; row < board.length; ++row)
+			for(int col = 0; col < board.length; ++col)
+			{
+				if(checkWinnerHelp(who, 5, row, col, 1, 0))
+					return true;
+				if(checkWinnerHelp(who, 5, row, col, 1, 1))
+					return true;
+				if(checkWinnerHelp(who, 5, row, col, 0, 1))
+					return true;
+			}
+		return false;
+	}
+
+	private boolean checkWinnerHelp(int who, int remain, int row, int col, int rowMove, int colMove){
+		if(remain == 0)
+			return true;
+		if(board[row][col] != who)
+			return false;
+		if(row + rowMove < 0 || row + rowMove > board.length - 1)
+			return false;
+		if(col + colMove < 0 || col + colMove > board.length - 1)
+			return false;
+		return checkWinnerHelp(who, remain - 1, row + rowMove, col + colMove, rowMove, colMove);
+	}
+
+	private boolean isFull(){
+		for(int row = 0; row < board.length; ++row)
+			for(int col = 0; col < board.length; ++col)
+				if(board[row][col] == 0)
+					return false;
+		return true;
+	}
+}

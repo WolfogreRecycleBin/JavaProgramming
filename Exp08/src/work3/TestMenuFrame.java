a39a990b5405dd5543e91bdb201bc8fa0d081a12
Jason Song <wolfogre@outlook.com>
2016-01-18 22:26:19 +0800

    Finish word3

---
+++ Exp08/src/work3/TestMenuFrame.java
@@ -0,0 +1,11 @@
+package work3;
+
+/**
+ * Created by Jason Song(wolfogre@outlook.com) on 01/18/2016.
+ */
+public class TestMenuFrame {
+	public static void main(String args[]){
+		MenuFrame menuFrame = new MenuFrame();
+		menuFrame.setVisible(true);
+	}
+}
